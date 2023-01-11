ace8089d00c3abd429210924c3db76369bcc7789
Elroy Reinhard <elroyreinhard02@gmail.com>
2023-01-12 01:38:32 +0700

    Latest Commit

--- app/src/main/java/com/example/mybox/BoxAdapter.kt
+++ app/src/main/java/com/example/mybox/BoxAdapter.kt
@@ -1,28 +1,32 @@
 package com.example.mybox
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
-class BoxAdapter(private val heroes: List<CategoryModel>,
-                 private val adapterOnClick: (CategoryModel) -> Unit) : RecyclerView.Adapter<BoxAdapter.BoxrHolder>() {
+class BoxAdapter(private val boxList: List<CategoryModel>, private val adapterOnClick: (CategoryModel) -> Unit) : RecyclerView.Adapter<BoxAdapter.BoxrHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): BoxrHolder {
         return BoxrHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout.list_card, viewGroup, false))
     }
 
-    override fun getItemCount(): Int = heroes.size
+    override fun getItemCount(): Int = boxList.size
 
     override fun onBindViewHolder(holder: BoxrHolder, position: Int) {
-        holder.bindHero(heroes[position])
+        holder.bindHero(boxList[position])
     }
 
-    inner class BoxrHolder(view: android.view.View) : RecyclerView.ViewHolder(view) {
+    inner class BoxrHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindHero(boxr: CategoryModel) {
             itemView.apply {
-                txtHeroName.text = boxr.Name
-                Picasso.get().load(boxr.ImageURL).into(imgHeroes)
+                var itemImage : ImageView = itemView.findViewById(R.id.ivRv)
+                var itemTitle : TextView = itemView.findViewById(R.id.textViewCard)
+                var itemCategory : TextView = itemView.findViewById(R.id.textViewCategoryCard)
+                Picasso.get().load(boxr.ImageURL).into(itemImage)
 
                 setOnClickListener {
                     adapterOnClick(boxr)
--- app/src/main/java/com/example/mybox/MainActivity.kt
+++ app/src/main/java/com/example/mybox/MainActivity.kt
@@ -4,20 +4,35 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
     private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var boxList: ArrayList<CategoryModel>
+    private lateinit var dbReference: DatabaseReference
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         floatingActionButton = findViewById(R.id.fAB1)
+        recyclerView = findViewById(R.id.rvMainPage)
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+
+        val boxAdapter = BoxAdapter(boxList) { boxr ->
+            Toast.makeText(applicationContext, "hehe", Toast.LENGTH_SHORT).show()
+        }
+
+        recyclerView.adapter = boxAdapter
+
         floatingActionButton.setOnClickListener{
             startActivity(Intent(this@MainActivity, SecondActivity::class.java))
 
